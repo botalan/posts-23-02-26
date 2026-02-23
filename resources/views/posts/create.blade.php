1de7e7b54367cebf8c1fb92730ffbd53e6e97ebc
adriansliberis <adriansliberis@Opti-06.localdomain>
2026-02-23 14:57:30 +0200

    post create form with submit to store method

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/posts" method="post">
+        @csrf
+        <div>
+            <label for="title" >Virsrakst</label><br>
+            <input type="text" name="title" id="title"><br>
+        </div>
+        <div>
+            <label for="content" >Saturs</label><br>
+            <textarea name="content" id="content"></textarea><br>
+        </div>
+        <div>
+            <input type="submit" value="Izveidot">
+        </div>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+
+Route::get('/posts/create', [PostController::class, "create"]);
+Route::post('/posts', [PostController::class, "create"]);
+
